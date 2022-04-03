5d3c3004128624e07217730946eea58bcf149d39
EmilioOcelotl <emilio.ocelotl@gmail.com>
2022-04-03 00:33:08 -0600

    sample cada 10

--- three2.0/index.js
+++ three2.0/index.js
@@ -5,10 +5,11 @@ import { GLTFLoader } from './jsm/loaders/GLTFLoader.js';
 import { DRACOLoader } from './jsm/loaders/DRACOLoader.js';
 import { ImprovedNoise } from './jsm/math/ImprovedNoise.js'; 
 
+/*
 const OSC = require('osc-js'); // pal osc 
 const osc = new OSC();
 osc.open();
-
+*/ 
 
 /* 
 document.querySelector('button').addEventListener('click', async () => {
@@ -31,6 +32,10 @@ let mouseY = 0;
 
 let windowHalfX = window.innerWidth / 2;
 let windowHalfY = window.innerHeight / 2;
+
+
+Tone.start().then( (x) => init() );
+
 
 document.addEventListener( 'mousemove', onDocumentMouseMove );
 
@@ -59,8 +64,6 @@ let controls;
 
 let aButton = false; 
 
-Tone.start().then( (x) => init());
-
 let fuentes;
 
 let meshpX = [];
@@ -75,8 +78,64 @@ console.log(onsetdetector);
 
 // init();
 
-function init(){
+// fuentes.player("2").start(5);
 
+an1 = new Tone.Analyser('fft', 32 ); 
+an1.smoothing = 0.8; 
+
+an2 = new Tone.Analyser('fft', 32 ); 
+an2.smoothing = 0.99; 
+
+an3 = new Tone.Analyser('fft', 32 ); 
+an3.smoothing = 0.99; 
+
+/*
+  mic = new Tone.UserMedia(); // Tendrá que ver con el volumen ? 
+  
+  mic.open().then(() => {
+  mic.connect( an1 ); 
+    });
+*/ 
+
+fuentes = new Tone.Players({
+    "0": "audio/cello.wav",
+    "1": "audio/wpa1.wav",
+    "2": "audio/wpa2.wav"
+}).toDestination();
+
+fuentes.player('0').connect( an1 ); 
+fuentes.player('1').connect( an2 ); 
+fuentes.player('2').connect( an3 ); 
+
+fuentes.volume.value = -6; 
+let clonadx;
+
+let meshes = []; 
+
+function init(){
+    
+    let loader = new GLTFLoader();
+    var dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath( '/js/draco/' );
+    loader.setDRACOLoader( dracoLoader );
+    
+    for(let i = 1; i < 5323; i = i + 20  ){
+	
+	loader.load(
+	    'glTF_Meshes/'+i+'.gltf', 
+	    function ( gltf ) {
+		meshes[contAnim] = gltf.scene.children[0];
+		contAnim++;
+	
+	    })
+
+	// console.log(contAnim, i); 
+	
+	
+    }
+    
+    // loadMeshes();
+    
     gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads() : []);
         
     document.body.style.cursor = 'none'; 
@@ -97,58 +156,77 @@ function init(){
     scene = new THREE.Scene();
     // scene.background = new THREE.Color(0xffffff); 
 
-    light = new THREE.PointLight( 0xffffff, 2, 10 );
-    light.position.set( 0, 0, 0 );
+    light = new THREE.PointLight( 0xffffff, 4, 20 );
+    light.position.set( 0, 0, -4 );
     scene.add( light );
     
-    light2 = new THREE.PointLight( 0xffffff, 2, 10 );
-    light2.position.set( 0, 0, 0 );
+    light2 = new THREE.PointLight( 0xffffff, 4, 20 );
+    light2.position.set( 0, 0, -4 );
     scene.add( light2 );
     
-    let loader = new GLTFLoader();
-    var dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderPath( '/js/draco/' );
-    loader.setDRACOLoader( dracoLoader );
+    if(gamepads){
+	console.log("hay gamepads"); 
+    }
+    	        
+    /*
+    mic = new Tone.UserMedia(); // Tendrá que ver con el volumen ? 
     
-    loader.load(
-	 'cap/2022-03-22--20-32-39.gltf',
-	//  'cap/0000000.gltf',
-	//'cap/prueba.glb', 
-	function ( gltf ) {
-	    //fuente.position.set(0, 0, 40)
-	    obj = gltf.scene.children[0];
-	    objeto(contAnim); 
-	    // console.log(obj.children.length );
+    mic.open().then(() => {
+	mic.connect( an1 ); 
+    });
+    */ 
+    // fuentes.volume.value = -6; 
 
-	    loopOf = new Tone.Loop((time) => {
-		
-		if(contAnim > obj.children.length -17 ) { // cuadros malos 
-		    contAnim = 0;
-		    console.log("hola"); 
-		}
-		
-		scene.remove(scene.children[3]);
-		
-		contAnim++;
-		
-		scene.add(obj.children[contAnim].clone() ); 
-		scene.children[3].rotation.y = Math.PI;
-		scene.children[3].position.z = -8;
-		scene.children[3].position.x = 4;
-		scene.children[3].material.size = 0.125;
-		// scene.children[3].material.blending = THREE.AdditiveBlending;
-		scene.children[3].scale.x = 32;
-		scene.children[3].scale.y = 32;
-		scene.children[3].scale.z = 32;
-		gltfBool = true; 
-		
-	    }, "0.3");
-	    
-	    Tone.Transport.start();
-	    loopOf.start(0);   
-	    
-	})
+    part();
     
+    // 523 
+    
+
+    // console.log(meshes[1]); 
+	    // scene.add(meshes[0]);
+    
+    /*
+    loader.load(
+	  'cap/0000000.gltf', 
+	function ( gltf ) {
+	    gltfBool = true;
+	    scene.add(gltf.scene.children[0]); 
+	    objeto(); 
+	    clonadx = scene.children[3].clone(); 
+	    obj.scale.x = 4;
+	    obj.scale.y = 4;
+	    obj.scale.z = 4;
+	    obj.rotation.y = Math.PI;
+	})
+    */
+
+    let contMesh = 0; 
+    
+    loopOf = new Tone.Loop((time) => {
+
+	if(contMesh == meshes.length){
+	    contMesh = 0;
+	    console.log("reseteo");
+	    loopOf.stop(); 
+	}
+
+	scene.remove(meshes[contMesh]); 
+	contMesh++;
+	meshes[contMesh].scale.x = 16;
+	meshes[contMesh].scale.y = 16;
+	meshes[contMesh].scale.z = 16;
+	meshes[contMesh].rotation.y = Math.PI;
+	//meshes[contMesh].material.roughness = 0.5; 
+	// meshes[contMesh].material.metalness = 0.2; 
+		
+	clonadx = meshes[contMesh].clone(); 
+
+	scene.add(meshes[contMesh]); 
+	
+    }, "0.77");
+    
+    objeto(); 
+
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -161,45 +239,7 @@ function init(){
     controls.target.set( 0, 0, 0 );
     controls.update();
     window.addEventListener( 'resize', onWindowResize );
-
-    if(gamepads){
-	console.log("hay gamepads"); 
-    }
-    
-    an1 = new Tone.Analyser('fft', 32 ); 
-    an1.smoothing = 0.8; 
-     
-    an2 = new Tone.Analyser('fft', 32 ); 
-    an2.smoothing = 0.99; 
- 
-    an3 = new Tone.Analyser('fft', 32 ); 
-    an3.smoothing = 0.99; 
-
-    /*
-    mic = new Tone.UserMedia(); // Tendrá que ver con el volumen ? 
-    
-    mic.open().then(() => {
-	mic.connect( an1 ); 
-    });
-    */ 
-
-    fuentes = new Tone.Players({
-	"0": "audio/cello.wav",
-	"1": "audio/wpa1.wav",
-	"2": "audio/wpa2.wav",
-	"3": "audio/mezclaFinal.mp3"
-    }).toDestination();
-
-    fuentes.player('0').connect( an1 ); 
-    fuentes.player('1').connect( an2 ); 
-    fuentes.player('2').connect( an3 ); 
-
-    fuentes.volume.value = -6; 
-    
-    // fuentes.player("2").start(5);
-    
-    part();
-    oscSend(); 
+    // oscSend(); 
     animate();
 
     
@@ -207,21 +247,59 @@ function init(){
     
 }
 
+
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
-function objeto (children){
-    console.log(obj.children[children]);
-    
-    fuentes.player('0').start(0); 
-    fuentes.player('1').start(0); 
-    fuentes.player('2').start(0); 
+async function objeto (){
 
-    gltfBool = true;
-    scene.add(obj.children[children].clone() );
+    // console.log(obj);
+
+    await sonido(); 
+    
+}
+
+// Más tiempo para la animación
+
+function sonido(){
+
+    return new Promise(resolve => {
+	setTimeout(() => {
+
+	    dosTres(); 
+	    
+	    fuentes.player('0').start();
+	    fuentes.player('1').start();
+	    fuentes.player('2').start();
+	    
+	}, 30000);
+
+    });
+}
+
+async function dosTres(){
+    await ochoSegundos(); 
+}
+
+function ochoSegundos(){
+       return new Promise(resolve => {
+	setTimeout(() => {
+
+	    console.log(meshes); 
+	    scene.add(meshes[1]);
+
+	    Tone.Transport.start();
+	    loopOf.start(0);
+	    console.log(scene);
+	    gltfBool= true; 
+    
+	    
+	}, 8239);
+
+    });
 }
 
 function animate(){
@@ -231,11 +309,12 @@ function animate(){
 
 function render() {
 
+    // console.log(scene.children[3]); 
     // var pads = navigator.getGamepads();
 
     // console.log(pads);
     
-    // if(pads[1]){
+    //o if(pads[1]){
 	
 	// console.log(pads[1].axes[0]);
 
@@ -244,65 +323,36 @@ function render() {
 
 	//camera.position.x += ( (pads[1].axes[0]*500) - camera.position.x ) * .25 * Math.cos( 0.25 );
 	//camera.position.y += ( - (pads[1].axes[1]*500) - camera.position.y ) * .25;
-	// camera.position.z +=  ( (pads[1].axes[3]*500) - camera.position.z ) * .25 * Math.cos( 0.25 ); 
+    // camera.position.z +=  ( (pads[1].axes[3]*500) - camera.position.z ) * .25 * Math.cos( 0.25 ); 
 	
 	// if(pads[1].buttons[7].pressed == true){ // algún tipo de protección para que no se repiita
 
     if(gltfBool){
 	aButton = true; 
-
-	    /*
-	    if(contAnim > obj.children.length -17 ) { // cuadros malos 
-		contAnim = 0;
-		console.log("hola"); 		    
+    	    for(let i = 0; i < scene.children[3].geometry.attributes.position.count; i++){
+		meshpX[i] = clonadx.geometry.attributes.position.getX(i);
+		meshpY[i] = clonadx.geometry.attributes.position.getY(i);
+		meshpZ[i] = clonadx.geometry.attributes.position.getZ(i);
 	    }
-	    
-	    scene.remove(scene.children[3]);	    
-	    contAnim++;
-	    scene.add(obj.children[contAnim].clone() ); 
-	    scene.children[3].rotation.y = Math.PI;
-	    scene.children[3].position.z = -8;
-	    scene.children[3].position.x = 4;
-	    scene.children[3].material.size = 0.125;
-	    // scene.children[3].material.blending = THREE.AdditiveBlending;
-	    scene.children[3].scale.x = 32;
-	    scene.children[3].scale.y = 32;
-	    scene.children[3].scale.z = 32; 
-	    */
-	    
-	    // console.log(scene.children[3].material.map)
-	    scene.children[2].geometry.attributes.color.needsUpdate = true;
-	    
-	    // esto podría estar asociado a un botón 
-
-	    for(let i = 0; i < scene.children[3].geometry.attributes.position.count; i++){
-		meshpX[i] = scene.children[3].geometry.attributes.position.getX(i);
-		meshpY[i] = scene.children[3].geometry.attributes.position.getY(i);
-		meshpZ[i] = scene.children[3].geometry.attributes.position.getZ(i);
-	    }
-	    
-	    for(let i = 0; i < scene.children[2].geometry.attributes.color.count; i++){
-		
-		scene.children[2].geometry.attributes.color.setXYZ(i,
-								   scene.children[3].geometry.attributes.color.getX(i),
-								   scene.children[3].geometry.attributes.color.getY(i),
-								   scene.children[3].geometry.attributes.color.getZ(i));   		    
-	    }   
-    // else {
-    //	    aButton = false; 
-    //}
-    // }
 	
-    const time = Date.now() * 0.0005;
-    const delta = clock.getDelta();
-	scene.children[3].geometry.computeVertexNormals(); 
+
+
+    // const time = Date.now() * 0.0005;
+    //const delta = clock.getDelta();
+
+	const time = Date.now() * 0.0005;
+	const delta = clock.getDelta();
+	
 	let perlin = new ImprovedNoise();
  
+	scene.children[3].geometry.computeVertexNormals(); 
+	// let perlin = new ImprovedNoise();
+ 
 	for( var i = 0; i < scene.children[3].geometry.attributes.position.count; i++){
-	    let d = perlin.noise(scene.children[3].geometry.attributes.position.getX(i)*8+time,
-				 scene.children[3].geometry.attributes.position.getY(i)*8+time,
-				 scene.children[3].geometry.attributes.position.getZ(i)*8+time ) * 0.1
-
+	    let d = perlin.noise(scene.children[3].geometry.attributes.position.getX(i)*0.01+time,
+				 scene.children[3].geometry.attributes.position.getY(i)*0.01+time,
+				 scene.children[3].geometry.attributes.position.getZ(i)*0.01+time ) * 0.1
+	    
 	    scene.children[3].geometry.attributes.position.setX(i, meshpX[i] * (d+1));
 	    scene.children[3].geometry.attributes.position.setY(i, meshpY[i] * (d+1));
 	    scene.children[3].geometry.attributes.position.setZ(i, meshpZ[i] * (d+1));
@@ -313,19 +363,16 @@ function render() {
 	scene.children[3].geometry.computeVertexNormals(); 
 
 	// PARTICULAS ///
-	// console.log((Tone.dbToGain(pos[10])) *10000000); 	
-	// console.log( Tone.dbToGain(an1.getValue()[0]) *100 ); 
 	
 	for( var i = 0; i < scene.children[2].geometry.attributes.position.count; i++){
-	   
-	    
+	   	    
 	    //let pos = an1.getValue();
 	    // console.log(Tone.dbToGain(pos[100])); 
 	    //console.log(Tone.dbToGain(pos[10])); 
 
-	    let d = perlin.noise(scene.children[2].geometry.attributes.position.getX(i)*(Tone.dbToGain(an1.getValue()[i%32] )*10000)+time,
-				 scene.children[2].geometry.attributes.position.getY(i)*(Tone.dbToGain(an2.getValue()[i%32] )*10000)+time,
-				 scene.children[2].geometry.attributes.position.getZ(i)*(Tone.dbToGain(an3.getValue()[i%32] )*10000)+time) * 1	    
+	    let d = perlin.noise(scene.children[2].geometry.attributes.position.getX(i)*(Tone.dbToGain(an1.getValue()[i%32] )*1000)+time,
+				 scene.children[2].geometry.attributes.position.getY(i)*(Tone.dbToGain(an2.getValue()[i%32] )*1000)+time,
+				 scene.children[2].geometry.attributes.position.getZ(i)*(Tone.dbToGain(an3.getValue()[i%32] )*1000)+time) * 1	    
 
 	    /*
 	    scene.children[2].geometry.attributes.position.setX(i,  (d+1) * pX[i]  * (Tone.dbToGain(an1.getValue()[i%32] ) * 200)* 10) 
@@ -341,30 +388,26 @@ function render() {
 	}
 	
 	scene.children[2].geometry.attributes.position.needsUpdate = true;
+
+	
+	//camera.position.x = Math.sin( time * 0.25 ) * ( 75 + Math.sin( time * 0.5 )* 10); 
+	//camera.position.y = Math.cos( time * 0.25 ) * 10; 
+	// camera.position.z = Math.cos( time * 0.25 ) * - 10;     
+	
+	camera.position.x += ( mouseX - camera.position.x ) * .25 * Math.cos( 0.25 );
+	camera.position.y += ( - mouseY - camera.position.y ) * .25;
 	
     }
 
-    /*
-    light.position.x = Math.sin( time * 6 ) * 0.5 - 2;
-    light.position.y = Math.cos( time * 3 ) * 0.75 ;
-    light.position.z = Math.cos( time * 7 ) * 0.5 -2;
-
-    light.position.x = Math.sin( time * -6 ) * 0.5 - 2;
-    light.position.y = Math.cos( time * -3 ) * 0.75 ;
-    light.position.z = Math.cos( time * -7 ) * 0.5 -2;
-    */
-    camera.position.x += ( mouseX - camera.position.x ) * .25 * Math.cos( 0.25 );
-    camera.position.y += ( - mouseY - camera.position.y ) * .25;
     
      //camera.rotation.y = Math.cos( time * 0.125 ) *
     camera.lookAt( 0, 0, 16 );
     renderer.render( scene, camera );
-    
-}
+}   
 
 function onDocumentMouseMove( event ) {
-    mouseX = ( event.clientX - windowHalfX ) / 4;
-    mouseY = ( event.clientY - windowHalfY ) / 4;
+    mouseX = ( event.clientX - windowHalfX ) / 16;
+    mouseY = ( event.clientY - windowHalfY ) / 16;
 }
 
 function part(){
@@ -380,14 +423,14 @@ function part(){
 	posY = Math.sin(theta1);
 	posZ = Math.cos(theta1) * Math.sin(theta2);
 	
-	let radio =45; 
+	let radio =24; 
 	
 	pX[i] = posX * radio ; 
 	pY[i] = posY * radio ;
 	pZ[i] = posZ * radio ; 
 	
 	vertices.push(posX * radio, posY*radio, posZ*radio);
-	colores.push(0, 0, 0); 
+	colores.push(255, 255, 255 ); 
 	    
     }
 
@@ -396,14 +439,16 @@ function part(){
     pGeo.setAttribute( 'color', new THREE.Float32BufferAttribute( colores, 3 ) );
     pMat = new THREE.PointsMaterial( { color: 0xffffff, vertexColors: true } );
     pointsPart = new THREE.Points( pGeo, pMat );
-    pMat.size =1;
-    pointsPart.position.z = 20; 
+    pMat.size =0.05;
+    pointsPart.position.z = 4; 
     scene.add( pointsPart );
 
     
     
 }
 
+
+/*
 function oscSend(){
     osc.on('open', () => {
 	setInterval(function(){
@@ -413,3 +458,4 @@ function oscSend(){
 	}, 100);
     }); 
 }
+*/
